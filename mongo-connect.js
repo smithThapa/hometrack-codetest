331e02dbeb7de835a68b50771f41fea050918c9a
Smith Thapa <smith.thapa@gmail.com>
2019-02-28 12:23:25 +1100

    Initial commit

---
+++ mongo-connect.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT || 3000;
+let db;
+
+MongoClient.connect('mongodb://localhost:27017/Payload', (err,client) => {
+    if (err){
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+    db = client.db('Payload');
+})
+
+app.use(bodyParser.json());
+
+app.post('/payload', (req, res) => {
+    let payloadArray = [];
+    let error = false;
+
+    req.body.payload.forEach(payload => {
+        db.collection('payload').insertOne(payload,
+             (err, result) => {
+                    if(err){
+                        error = true;
+                        return console.log('Unable to insert payload', err);
+                    }
+                })
+    })
+
+    if(!error){
+        db.collection('payload').find(
+            {"type": "htv",
+            "workflow": "completed"}).toArray().then((docs) => {
+                for (let index = 0; index < docs.length; index++) {
+                    let buildingNumber = docs[index].address.buildingNumber;
+                    let street = docs[index].address.street;
+                    let suburb = docs[index].address.suburb;
+                    let postcode = docs[index].address.postcode;
+
+                    payloadArray[index] = {
+                        "concataddress": `${buildingNumber} ${street} ${suburb} ${postcode}`, 
+                        "type": "htv",
+                        "workflow": "completed"
+                    }
+                    
+                }
+
+                let responseObj = {
+                    "response": payloadArray
+                }
+                console.log("yolo");
+                console.log(JSON.stringify(responseObj,undefined,2));
+            }, (err) => {
+                console.log('Unable to fetch payloads',err);
+            })
+    }
+})
+
+app.listen(port, () => {
+    console.log(`Started on port ${port}`);
+})
